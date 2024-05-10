1735a6887d53a197da308d70a54b6800fe0f2cff
KIMI_7 <210300533@ucaribe.edu.mx>
2024-05-10 14:12:59 -0500

    Cambios

--- Abarrotes_Interfaz.py
+++ Abarrotes_Interfaz.py
@@ -1,30 +1,26 @@
-import tkinter as tk
 import customtkinter as ctk
 from PIL import Image
 
-ctk.set_appearance_mode("system")
-ctk.set_default_color_theme("blue")
+class VentanaPrincipal:
+    def __init__(self, master):
+        self.master = master
+        self.master.geometry("500x600+350+20")
+        self.master.title("Simulador de Abarrotes By Team 9")
+        
+        self.my_image = ctk.CTkImage(light_image=Image.open('images/Logo1.jpg'),
+                                      dark_image=Image.open('images/Logo1.jpg'),
+                                      size=(200, 200))
+        self.my_label = ctk.CTkLabel(self.master, text='', image=self.my_image)
+        self.my_label.pack(pady=10)
+        self.my_label.place(relx=0.5, rely=0.25, anchor=ctk.CENTER)
 
-# Primera Ventana
-root = ctk.CTk()
-root.geometry("500x600+350+20")
-root.title("Simulador de Abarrotes By Team 9")
+        User = ["Salvador", "Israel", "Francisco"]
+        self.my_box = ctk.CTkComboBox(self.master, values=User)
+        self.my_box.pack(pady=20)
+        self.my_box.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
 
-my_image = ctk.CTkImage(light_image=Image.open('images/Logo1.jpg'),
-                        dark_image=Image.open('images/Logo1.jpg'),
-                        size=(200, 200))
-my_label = ctk.CTkLabel(root, text='', image=my_image)
-my_label.pack(pady=10)
-my_label.place(relx=0.5, rely=0.25, anchor=ctk.CENTER)
-
-User = ["Salvador", "Israel", "Francisco"]
-my_box = ctk.CTkComboBox(root, values=User)
-my_box.pack(pady=20)
-my_box.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
-
-my_label1 = ctk.CTkLabel(root, text='© Copyright by Kimi')
-my_label1.place(relx=0.5, rely=0.95, anchor=ctk.CENTER)
-
+        self.my_label1 = ctk.CTkLabel(self.master, text='© Copyright by Kimi')
+        self.my_label1.place(relx=0.5, rely=0.95, anchor=ctk.CENTER)
 
 class Ventana_Simulador(ctk.CTkToplevel):
     def __init__(self, master=None, perfil=''):
@@ -35,18 +31,19 @@ class Ventana_Simulador(ctk.CTkToplevel):
         self.Label1 = ctk.CTkLabel(self, text=f'¡Bienvenido, {perfil}!')
         self.Label1.pack()
 
-
 def login():
-    perfil = my_box.get()  # Obtener el perfil seleccionado
-    root.destroy()  # Cerrar la ventana actual
+    perfil = app.my_box.get()  # Obtener el perfil seleccionado
+    root.destroy()  
     Ventana_Simulador(perfil=perfil)  # Crear la nueva ventana
 
+# Crear la ventana principal y la instancia de la clase VentanaPrincipal
+root = ctk.CTk()
+app = VentanaPrincipal(root)
 
 button = ctk.CTkButton(master=root, text="Iniciar Simulacion", command=login, corner_radius=32, fg_color='#4158D0',
                        hover_color='#7f5af0')
 button.place(relx=0.5, rely=0.6, anchor=ctk.CENTER)
 
 root.mainloop()
-
 
 #Prueba 1
